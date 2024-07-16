921251d468e72cea3343688d243613cf844c93c5
bveak <2285135385@qq.com>
2024-07-16 20:23:22 +0800

    task1 ver2

--- Task1/add.cu
+++ Task1/add.cu
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <cassert>
 #include <cuda_runtime.h>
+#include <stdexcept>
 
 #define Float4(val) *(float4*)(&(val))
 
@@ -10,15 +11,59 @@ __global__ void reduce_sum(const float* input_vecs, std::size_t n, std::size_t d
     if (i < dim) {
         float4 sum, a;
         sum.x = sum.y = sum.z = sum.w = 0;
+        #pragma unroll
         for (int j = 0; j < n; ++j) {
-            a = Float4(input_vecs[j * dim + i]);
+            a = *((float4*)(input_vecs + j * dim + i));
             sum.x += a.x;
             sum.y += a.y;
             sum.z += a.z;
             sum.w += a.w;
         }
-        Float4(output_vec[i]) = sum;
+        *((float4*)(output_vec + i)) = sum;
     }
+}
+
+__inline__ __device__ float branch_warp_reduce_sum(float val) {
+    unsigned mask = __activemask();
+    for (int offset = warpSize / 2; offset > 0; offset /= 2) {
+        val += __shfl_down_sync(mask, val, offset);
+    }
+    return val;
+}
+
+__global__ void reduce_sum_dim1(const float* input_vecs, std::size_t n, std::size_t dim, float* output_vec) {
+    __shared__ float sum;
+    if (threadIdx.x == 0) sum = 0;
+    __syncthreads();
+    float local_sum = 0;
+    for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x) {
+        local_sum += __ldg(input_vecs + i);
+    }
+    local_sum = branch_warp_reduce_sum(local_sum);
+    if (threadIdx.x % warpSize == 0) 
+        atomicAdd(&sum, local_sum);
+    __syncthreads();
+    if (threadIdx.x == 0)
+        atomicAdd(output_vec, sum);
+}
+
+__global__ void reduce_sum_small(const float* input_vecs, std::size_t n, std::size_t dim, float* output_vec) {
+    const int TILE_SIZE = 32;
+    __shared__ float sum[TILE_SIZE];
+    if (threadIdx.x < TILE_SIZE)
+        sum[threadIdx.x] = 0;
+    __syncthreads();
+    float local_sum = 0;
+    for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n * dim; i += blockDim.x * gridDim.x) {
+        local_sum += __ldg(input_vecs + i);
+    }
+    atomicAdd(sum + threadIdx.x % TILE_SIZE, local_sum);
+    __syncthreads();
+    if (threadIdx.x < TILE_SIZE && dim <= threadIdx.x)
+        atomicAdd(sum + threadIdx.x % dim, sum[threadIdx.x]);
+    __syncthreads();
+    if (threadIdx.x < dim)
+        atomicAdd(output_vec + threadIdx.x, sum[threadIdx.x]);
 }
 
 void reduce(float& total_time, const float* input_vecs, std::size_t n, std::size_t dim, float* output_vec) {
@@ -27,12 +72,20 @@ void reduce(float& total_time, const float* input_vecs, std::size_t n, std::size
     float* output = NULL;
     assert(cudaMalloc((void**)&output, dim * sizeof(float)) == cudaSuccess);
     assert(cudaMemcpy(input, input_vecs, n * dim * sizeof(float), cudaMemcpyHostToDevice) == cudaSuccess);
-    int threadsPerBlock = 32;
+    int threadsPerBlock = 1024;
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start);
-    reduce_sum<<<(dim - 1) / (threadsPerBlock / 4) + 1, threadsPerBlock>>>(input, n, dim, output);
+    assert(cudaMemset(output, 0, dim * sizeof(float)) == cudaSuccess);
+    if (dim >= threadsPerBlock * 4) {
+        reduce_sum<<<(dim - 1) / (threadsPerBlock * 4) + 1, threadsPerBlock>>>(input, n, dim, output);
+    } else if (dim == 1) {
+        reduce_sum_dim1<<<multiProcessorCount, threadsPerBlock>>>(input, n, dim, output);
+    } else {
+        if (dim > 32 || (dim & -dim) != dim) throw std::invalid_argument("Not impl");
+        reduce_sum_small<<<multiProcessorCount, threadsPerBlock>>>(input, n, dim, output);
+    }
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     assert(cudaGetLastError() == cudaSuccess);
--- Task1/main.cu
+++ Task1/main.cu
@@ -23,26 +23,32 @@ void warmup() {
         rndval ^= rndval << 17;
     }
     assert(cudaMemcpy(test, rnd, 1024 * sizeof(int), cudaMemcpyHostToDevice) == cudaSuccess);
-    for (int i = 0; i < 8; ++i)
+    for (int i = 0; i < 8; ++i) {
         warmup_kernel<<<1024, 1024>>>(test);
+        assert(cudaGetLastError() == cudaSuccess);
+    }
     assert(cudaMemcpy(rnd, test, 1024 * sizeof(int), cudaMemcpyDeviceToHost) == cudaSuccess);
 }
 
 int main() {
-    std::size_t n = 50000;
-    std::size_t dim = 16384;
+    std::size_t n = 50000 << 9;
+    std::size_t dim = 32;
     std::mt19937 rnd((0x3ac2ed7b));
     float* input = (float*)malloc(n * dim * sizeof(float));
     for (int i = 0; i < n * dim; ++i)
         input[i] = rnd() / 1e4;
     float* output = (float*)malloc(dim * sizeof(float));
-    float total_time = 0;
+    float total_time = 0, max_time = 0, min_time = 1e5;
     warmup();
     int TestCount = 10;
     for (int i = 0; i < TestCount; ++i) {
-        reduce(total_time, input, n, dim, output);
+        float current_time = 0;
+        reduce(current_time, input, n, dim, output);
+        max_time = std::max(max_time, current_time);
+        min_time = std::min(min_time, current_time);
+        total_time += current_time;
     }
-    printf("GPU average time(ms): %f\n", total_time / TestCount);
+    printf("GPU average time(ms): %f\n", (total_time - max_time - min_time) / (TestCount - 2));
     float* ansput = (float*)malloc(dim * sizeof(float));
     for (int i = 0; i < dim; ++i)
         ansput[i] = 0;
@@ -53,8 +59,9 @@ int main() {
     }
     time_t end = clock();
     printf("CPU time(ms): %f\n", 1.0 * (end - start) / CLOCKS_PER_SEC * 1e3);
+    // printf("%f %f\n", ansput[0], output[0]);
     for (int i = 0; i < dim; ++i)
-        assert(std::fabs(ansput[i] - output[i]) < 1e-5);
+        assert(std::fabs((ansput[i] - output[i]) / ansput[i]) < 1e-3);
     puts("Accepted!!");
     free(input);
     free(output);
