06a4041721bf11549d18f70de466e6ba85d94aa3
bveak <2285135385@qq.com>
2024-07-16 22:21:08 +0800

    task3 v2

--- Task3/main.cu
+++ Task3/main.cu
@@ -4,6 +4,9 @@
 #include <random>
 #include <cmath>
 #include <algorithm>
+#include <thrust/sort.h>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
 
 __global__ void warmup_kernel(int* test) {
     int i = 1, j = 1;
@@ -29,25 +32,51 @@ void warmup() {
     assert(cudaMemcpy(rnd, test, 1024 * sizeof(int), cudaMemcpyDeviceToHost) == cudaSuccess);
 }
 int main() {
-    std::size_t n = 16 * 1024 * 1024;
+    std::size_t n = 1 << 28;
     std::vector <int> a(n);
     std::mt19937 rnd(0x3ac2ed7b);
     for (int i = 0; i < n; ++i) 
         a[i] = rnd();
     warmup();
-    float total_time = 0;
+    float total_time = 0, max_time = 0, min_time = 1e5;
     int TestCount = 10;
     std::vector <int> b;
     for (int i = 0; i < TestCount; ++i) {
         b = a;
-        sort(total_time, b);
+        float current_time = 0;
+        sort(current_time, b);
+        max_time = std::max(max_time, current_time);
+        min_time = std::min(min_time, current_time);
+        total_time += current_time;
     }
-    printf("GPU average time(ms): %f\n", total_time / TestCount);
-    time_t start = clock();
-    std::sort(a.begin(), a.end());
-    time_t end = clock();
-    printf("CPU time(ms): %f\n", 1.0 * (end - start) / CLOCKS_PER_SEC * 1e3);
-    assert(a == b);
+    printf("GPU average time(ms): %f\n", (total_time - max_time - min_time) / (TestCount - 2));
+    total_time = 0, max_time = 0, min_time = 1e5;
+    std::vector <int> c = a;
+    for (int i = 0; i < TestCount; ++i) {
+        // thrust::host_vector <int> arr = a;
+        thrust::device_vector <int> arr = a;
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        cudaEventRecord(start);
+        thrust::sort(arr.begin(), arr.end());
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        assert(cudaGetLastError() == cudaSuccess);
+        thrust::copy(arr.begin(), arr.end(), c.begin());
+        float current_time = 0;
+        cudaEventElapsedTime(&current_time, start, stop);
+        max_time = std::max(max_time, current_time);
+        min_time = std::min(min_time, current_time);
+        total_time += current_time;
+    }
+    printf("thrust average time(ms): %f\n", (total_time - max_time - min_time) / (TestCount - 2));
+    // time_t start = clock();
+    // std::sort(a.begin(), a.end());
+    // time_t end = clock();
+    // printf("CPU time(ms): %f\n", 1.0 * (end - start) / CLOCKS_PER_SEC * 1e3);
+    printf("CPU ommited\n");
+    assert(b == c);
     puts("Accepted!!");
     return 0;
 }
--- Task3/sort.cu
+++ Task3/sort.cu
@@ -10,31 +10,65 @@ __device__ void swap(int& a, int& b) {
     a ^= b ^= a ^= b;
 }
 
-__global__ void bitonic_sort(int* arr, int i, int j) {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    int id_comp = id ^ j;
-    if (id > id_comp)
-        if ((arr[id] < arr[id_comp]) == !(id & i))
-            swap(arr[id], arr[id_comp]);
+__global__ void bitonic_sort_simple(int* arr, int i, int j, int n) {
+    for (int id = blockIdx.x * blockDim.x + threadIdx.x; id < n; id += blockDim.x * gridDim.x) {
+        int id_comp = id ^ j;
+        if (id > id_comp)
+            if ((arr[id] < arr[id_comp]) == !(id & i))
+                swap(arr[id], arr[id_comp]);
+    }
 }
 
 __global__ void bitonic_sort_small(int* arr, int n) {
-    __shared__ int a[1024];
+    __shared__ int a[8192];
     int id = threadIdx.x;
-    a[id] = arr[blockIdx.x * blockDim.x + id];
-    __syncthreads();
-    for (int i = 2; i <= 1024; i *= 2) {
-        bool chk = id & i;
-        if (i == 1024) chk = blockIdx.x & 1;
-        for (int j = i / 2; j; j /= 2) {
-            int id_comp = id ^ j;
-            if (id > id_comp)
-                if ((a[id] < a[id_comp]) != chk)
-                    swap(a[id], a[id_comp]);
+    for (int pos = blockIdx.x * (blockDim.x * 8); pos < n; pos += (blockDim.x * 8) * gridDim.x) {
+        #pragma unroll
+        for (int j = 0; j < 8192; j += 1024)
+            a[id + j] = arr[pos + id + j];
+        __syncthreads();
+        for (int i = 2; i <= 8192; i *= 2) {
+            #pragma unroll
+            for (int j = i / 2; j; j /= 2) {
+                #pragma unroll
+                for (int p = id; p < 4096; p += 1024) {
+                    int lb = p & (j - 1);
+                    int u = (p ^ lb) << 1 | lb, v = u | j;
+                    if ((a[v] < a[u]) == !((u | pos) & i)) 
+                        swap(a[v], a[u]);
+                }
+                __syncthreads();
+            }
+        }
+        #pragma unroll
+        for (int j = 0; j < 8192; j += 1024)
+            arr[pos + id + j] = a[id + j];
+    }
+}
+
+__global__ void bitonic_sort_large(int* arr, int i, int n) {
+    __shared__ int a[8192];
+    int id = threadIdx.x;
+    for (int pos = blockIdx.x * (blockDim.x * 8); pos < n; pos += (blockDim.x * 8) * gridDim.x) {
+        #pragma unroll
+        for (int j = 0; j < 8192; j += 1024)
+            a[id + j] = arr[pos + id + j];
+        __syncthreads();
+        #pragma unroll
+        for (int j = 4096; j; j /= 2) {
+            #pragma unroll
+            for (int p = id; p < 4096; p += 1024) {
+                int lb = p & (j - 1);
+                int u = (p ^ lb) << 1 | lb, v = u | j;
+                if ((a[v] < a[u]) == !((u | pos) & i)) 
+                    swap(a[v], a[u]);
+            }
             __syncthreads();
         }
+        #pragma unroll
+        for (int j = 0; j < 8192; j += 1024)
+            arr[pos + id + j] = a[id + j];
     }
-    arr[blockIdx.x * blockDim.x + id] = a[id];
 }
 
 void sort(float& total_time, std::vector <int> &nums) {
@@ -47,10 +81,12 @@ void sort(float& total_time, std::vector <int> &nums) {
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start);
-    bitonic_sort_small<<<(n - 1) / threadSize + 1, threadSize>>>(input, n);
-    for (int i = 2048; i <= n; i *= 2)
-        for (int j = i / 2; j; j /= 2) 
-            bitonic_sort<<<(n - 1) / threadSize + 1, threadSize>>>(input, i, j);
+    bitonic_sort_small<<<multiProcessorCount, threadSize>>>(input, n);
+    for (int i = 16384; i <= n; i *= 2) {
+        for (int j = i / 2; j >= 8192; j /= 2) 
+            bitonic_sort_simple<<<multiProcessorCount, threadSize>>>(input, i, j, n);
+        bitonic_sort_large<<<multiProcessorCount, threadSize>>>(input, i, n);
+    }
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     assert(cudaGetLastError() == cudaSuccess);
