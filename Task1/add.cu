128d221919506a1b71eaf0b0b38604dd92beb30c
bveak <2285135385@qq.com>
2024-07-16 11:19:06 +0800

    use float4

--- Task1/add.cu
+++ Task1/add.cu
@@ -3,13 +3,21 @@
 #include <cassert>
 #include <cuda_runtime.h>
 
+#define Float4(val) *(float4*)(&(val))
+
 __global__ void reduce_sum(const float* input_vecs, std::size_t n, std::size_t dim, float* output_vec) {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    int i = 4 * (blockDim.x * blockIdx.x + threadIdx.x);
     if (i < dim) {
-        float sum = 0;
-        for (int j = 0; j < n; ++j) 
-            sum += input_vecs[j * dim + i];
-        output_vec[i] = sum;
+        float4 sum, a;
+        sum.x = sum.y = sum.z = sum.w = 0;
+        for (int j = 0; j < n; ++j) {
+            a = Float4(input_vecs[j * dim + i]);
+            sum.x += a.x;
+            sum.y += a.y;
+            sum.z += a.z;
+            sum.w += a.w;
+        }
+        Float4(output_vec[i]) = sum;
     }
 }
 
@@ -19,12 +27,12 @@ void reduce(float& total_time, const float* input_vecs, std::size_t n, std::size
     float* output = NULL;
     assert(cudaMalloc((void**)&output, dim * sizeof(float)) == cudaSuccess);
     assert(cudaMemcpy(input, input_vecs, n * dim * sizeof(float), cudaMemcpyHostToDevice) == cudaSuccess);
-    int threadsPerBlock = 1024;
+    int threadsPerBlock = 32;
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start);
-    reduce_sum<<<(dim - 1) / threadsPerBlock + 1, threadsPerBlock>>>(input, n, dim, output);
+    reduce_sum<<<(dim - 1) / (threadsPerBlock / 4) + 1, threadsPerBlock>>>(input, n, dim, output);
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     assert(cudaGetLastError() == cudaSuccess);
